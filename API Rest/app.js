8fd7c4673e0b41b6cee6c620b7edcf826f874bae
Maximiliano A. Romero Piaggio <maxiromeropiaggio@gmail.com>
2021-06-26 01:34:49 -0300

    * Agregado de archivos de angular. * Separación de la instanciación del server con la app. ** Falta completar la lógica de ./public/src/app.js ** está comentada. * package.json actualizados

--- API Rest/app.js
+++ API Rest/app.js
@@ -1,25 +1,48 @@
-import pkg_mongoose from 'mongoose';
-const { connect } = pkg_mongoose;
-import express from 'express';
 import pkg_bodyparser from 'body-parser';
-const { json, urlencoded } = pkg_bodyparser;
+import express from 'express';
+import createError from 'http-errors';
+import pkg_mongoose from 'mongoose';
+import path from 'path';
+import { dirname } from 'path';
+import index from './routes/index.route.js';
 import transactions from './routes/transaction.route.js';
 import users from './routes/user.route.js';
+const { connect } = pkg_mongoose;
+const { json, urlencoded } = pkg_bodyparser;
 
 
-connect('mongodb://localhost:27017/test', function (err) {
-   if (err) throw err;
-   console.log('Successfully connected to mongoDB.');
+connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true} ,function (err) {
+  if (err) throw err;
+  console.log('Successfully connected to mongoDB.');
 });
 
 var app = express();
-app.use(json()); 
-app.use(urlencoded({ extended: false })); 
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(express.static(path.join(dirname(import.meta.url), 'public')));
 
+// view engine setup
+app.set('views', path.join(dirname(import.meta.url), 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', index)
 app.use('/transactions', transactions);
 app.use('/users', users);
 
-app.listen(3000);
-console.log("Listening at port 3000 to requirements...");
+// catch 404 and forward to error handler
+app.use(function (_req, _res, next) {
+  next(createError(404));
+});
 
+// error handler
+app.use(function (err, req, res, _next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app;
---
+++ API Rest/routes/index.route.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+var router = Router();
+
+router.get('/',(req,res)=>{
+    res.render('index', { title: 'Express' });
+});
+
+export default router;
