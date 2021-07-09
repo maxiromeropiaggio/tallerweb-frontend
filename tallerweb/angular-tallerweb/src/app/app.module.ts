b9abc61481268f704d80f9addaaddb3adb3dfc68
Maximiliano A. Romero Piaggio <maxiromeropiaggio@gmail.com>
2021-07-09 16:25:30 -0300

    * Agregado de más icons * Cambio directorio de transaction-manager a components * Avance en la pestaña de transacciones

--- tallerweb/angular-tallerweb/src/app/app.module.ts
+++ tallerweb/angular-tallerweb/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TransactionManagerComponent } from './transaction-manager/transaction-manager.component';
+import { TransactionManagerComponent } from './components/transaction-manager/transaction-manager.component';
 
 @NgModule({
   declarations: [
